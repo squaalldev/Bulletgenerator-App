dfb7bec5041a4528d356c6ce1b4283c80202f3dd
Jesus Cabrera <JeCabrera@users.noreply.huggingface.co>
2024-09-21 05:51:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ def to_markdown(text):
     text = text.replace('•', '  *')  # Convertir los puntos en listas con asteriscos
     return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-def generate_headlines(number_of_headlines, target_audience, product, temperature):
+def generate_bullets(number_of_bullets, target_audience, product, temperature):
     # Crear la configuración del modelo
     generation_config = {
-        "temperature": temperature,  # Usar el valor del slider aquí
+        "temperature": temperature,
         "top_p": 0.95,
         "top_k": 64,
         "max_output_tokens": 2048,
@@ -28,7 +28,7 @@ def generate_headlines(number_of_headlines, target_audience, product, temperatur
     model = genai.GenerativeModel(
         model_name="gemini-1.5-flash",
         generation_config=generation_config,
-        system_instruction="Eres un copywriter de clase mundial, con experiencia en la creación de ganchos, titulares y líneas de asunto que capturan la atención de inmediato. Tu habilidad radica en comprender profundamente las emociones, deseos y desafíos de una audiencia específica, lo que te permite diseñar estrategias de marketing personalizadas que resuenan y motivan la acción. Sabes cómo utilizar estructuras probadas para atraer a tu audiencia objetivo, generando interés y logrando una conexión poderosa que impulsa los resultados deseados en campañas publicitarias y de contenido. Responde en español, en tipo lista numerada. Haz ganchos inusuales que atrapen la atención. No menciones el producto directamente en el gancho. No expliques el gancho o encabezado. Al responder escribe un encabezado que diga: 'Estos son tus encabezados para enganchar a {target_audience}'."
+        system_instruction="Eres un copywriter de clase mundial, con experiencia en la creación de beneficios que conectan síntomas con problemas. Tu habilidad radica en comprender profundamente las emociones, deseos y desafíos de una audiencia específica, lo que te permite diseñar estrategias de marketing personalizadas que resuenan y motivan la acción. Sabes cómo utilizar estructuras probadas para atraer a tu audiencia objetivo, generando interés y logrando una conexión poderosa que impulsa los resultados deseados en campañas publicitarias y de contenido. Responde en español, en tipo lista numerada. Haz bullets inusuales, creativos y fascinantes que atrapen la atención de los lectores. No menciones el producto directamente en los beneficios o bullets. No expliques los benficios o bullets. Al responder escribe un encabezado que diga: 'Estos son tus bullets para convencer a {target_audience}'."
     )
 
     chat_session = model.start_chat(
@@ -36,35 +36,35 @@ def generate_headlines(number_of_headlines, target_audience, product, temperatur
             {
                 "role": "user",
                 "parts": [
-                    f"Tu tarea es crear {number_of_headlines} ganchos o encabezados titulares llamativos diseñados para {target_audience} con el fin de generar interés en {product}. "
-                    "La idea es que los ganchos sean de este tipo: "
-                    "1. Secretos: 'El secreto detrás de...'; "
-                    "2. Consejos: 'Consejos para que...'; "
-                    "3. Historias: 'La historia del...', 'Los misterios de...', 'La leyenda de...'; "
-                    "4. Deseos: 'Cómo...'; "
-                    "5. Listas: '10 razones por las que...'; "
-                    "6. Haciendo una pregunta: '¿Sabías que...'; "
-                    "7. Curiosidad: '¿Por qué...'."
+                    f"Tu tarea es crear {number_of_bullets} beneficios o bullets que conecten el síntoma con el problema que tienen los {target_audience}, y que incrementen el deseo de adquirir el {product}"
+                    "La idea es que los bullets sean de este tipo: "
+                    "* Bien y mal: 'Botiquín del baño es el mejor lugar para guardar la medicina, ¿verdad? Incorrecto Es el peor. Los hechos están en la página 10.' "
+                    "* El mejor/El Peor: 'El mejor tiempo verbal que existe para dar la sensación a tus clientes que ya te han comprado.' "
+                    "* Historias: 'La historia del...', 'Los misterios de...', 'La leyenda de...' "
+                    "* Truco: 'Un sistema tonto para escribir copy sin tratar de convencer de que me compren.' "
+                    "* El de la verdad: 'La verdad que nunca te han dicho en el colegio, la escuela, ni en tu casa de cómo vivir de la música.' "
+                    "* Haciendo una pregunta: '¿Sabías que...' "
+                    "* Cuando: '¿Cuándo es buena idea decirle a una chica que te gusta? Si no lo dices justo en ese momento, despídete de que la conozcas íntimamente.' "
                 ],
             },
         ]
     )
 
-    response = chat_session.send_message("Genera los titulares")  # Enviar mensaje para obtener la respuesta
+    response = chat_session.send_message("Genera los beneficios o bullets")  # Enviar mensaje para obtener la respuesta
     return to_markdown(response.text)  # Usar to_markdown para formatear la respuesta
 
 # Configurar la interfaz de usuario con Gradio
 iface = gr.Interface(
-    fn=generate_headlines,
+    fn=generate_bullets,
     inputs=[
-        gr.Dropdown(choices=[str(i) for i in range(1, 11)], label="Número de Titulares", value="5"),
+        gr.Dropdown(choices=[str(i) for i in range(1, 11)], label="Número de Bullets", value="5"),
         gr.Textbox(label="Público Objetivo", placeholder="Ejemplo: Estudiantes Universitarios"),
         gr.Textbox(label="Producto", placeholder="Ejemplo: Curso de Inglés"),
         gr.Slider(minimum=0, maximum=1, value=0, step=0.1, label="Creatividad")
     ],
-    outputs=gr.Markdown(label="Titulares Generados"),  # Eliminado el placeholder
-    title="Generador de Titulares",
-    description="Usa el poder de Gemini AI para crear titulares atractivos. Ajusta los parámetros para generar titulares que capturen la atención de tu audiencia."
+    outputs=gr.Markdown(label="Bullets Generados"),
+    title="Generador de Bullets",
+    description="Usa el poder de Gemini AI para crear bullets atractivos que conecten síntomas con problemas. Ajusta los parámetros para generar bullets que capturen la atención de tu audiencia."
 )
 
 # Lanza la interfaz
