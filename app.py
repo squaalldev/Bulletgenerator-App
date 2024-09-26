386805c8c1be1846b70b5ce49690294596dd7530
Jesus Cabrera <JeCabrera@users.noreply.huggingface.co>
2024-09-26 00:21:33 +0000

    Update app.py

--- app.py
+++ app.py
@@ -139,14 +139,15 @@ if submit:
         try:
             # Obtener la respuesta del modelo
             generated_bullets = get_gemini_response_bullets(target_audience, product, num_bullets, creativity)
+            # Reemplazar saltos de línea con <br> para HTML
+            generated_bullets_html = generated_bullets.replace("\n", "<br>")
             # Insertar el contenido generado dentro del div
             col2.markdown(f"""
                 <div style="border: 1px solid #000000; padding: 5px; border-radius: 8px; background-color: #ffffff;">
-                    {generated_bullets}
+                    {generated_bullets_html}
                 </div>
             """, unsafe_allow_html=True)
         except ValueError as e:
             col2.error(f"Error: {str(e)}")
     else:
         col2.error("Por favor, proporciona el público objetivo y el producto.")
-
