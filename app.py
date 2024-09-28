b1d0e81ee05b2b073f11166d88ca928ba2d21812
Jesus Cabrera <JeCabrera@users.noreply.huggingface.co>
2024-09-28 06:32:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -34,75 +34,64 @@ def get_mention_instruction(product_mention, product):
 
 # Función para obtener una cantidad de bullets
 def get_gemini_response_bullets(target_audience, product, num_bullets, temperature):
-    generated_bullets_set = set()  # Usar un conjunto para almacenar los bullets únicos
-    bullets = []
+    product_mention = get_random_product_mention()
+    mention_instruction = get_mention_instruction(product_mention, product)  # Define aquí
+    model_choice = "gemini-1.5-flash"  # Modelo por defecto
 
-    while len(bullets) < num_bullets:
-        product_mention = get_random_product_mention()
-        mention_instruction = get_mention_instruction(product_mention, product)
-        
-        model_choice = "gemini-1.5-flash"  # Modelo por defecto
-
-        # Configuración del modelo generativo y las instrucciones del sistema
-        model = genai.GenerativeModel(
-            model_name=model_choice,
-            generation_config={
-                "temperature": temperature,
-                "top_p": 0.85,
-                "top_k": 128,
-                "max_output_tokens": 2048,
-                "response_mime_type": "text/plain",
-            },
-            system_instruction=(
-                f"You are a world-class copywriter, expert in creating benefits that connect symptoms with problems of {target_audience}. "
-                f"You deeply understand the emotions, desires, and challenges of {target_audience}, allowing you to design personalized copywriting that resonate and motivate action. "
-                f"You know how to use proven structures to attract {target_audience}, generating interest and creating a powerful connection with {product}. "
-                "Generate unusual, creative, and fascinating bullets that capturing {target_audience}'s attention. Respond in Spanish and use a numbered list format. "
-                "Important: Never include explanations or categories, like this: 'La leyenda del padre soltero: Dice que nunca hay tiempo suficiente. El yoga te enseña a usar mejor el tiempo que tienes, incluso cuando te parece imposible.' "
-                "Los bullets deben ser de los siguientes tipos: "
-                "* 'El armario del baño es el mejor lugar para guardar medicamentos, ¿verdad? Incorrecto. Es el peor. Los hechos están en la página 10.' "
-                "* 'El mejor tiempo verbal que le da a tus clientes la sensación de que ya te han comprado.' "
-                "* 'La historia de...', 'Los misterios de...', 'La leyenda de...' "
-                "* 'Un sistema simple para escribir textos sin intentar convencerlos de comprar.' "
-                "* Verdad: 'La verdad que nunca te han contado en la escuela, o en casa, sobre cómo ganarte la vida con la música.' "
-                "* '¿Sabías que...' "
-                "* '¿Cuándo es una buena idea decirle a una chica que te gusta? Si no se lo dices en ese momento, despídete de conocerla íntimamente.' "
-                "Recuerda que un buen bullet debe tener:\n\n"
-                "1. **Conexión**: Palabras que resalten la relación entre el producto y el beneficio para el usuario (e.g., 'Mejorar', 'Transformar').\n"
-                "2. **Beneficio**: Explicar cómo el usuario se beneficiará al asistir, descargar o comprar el producto.\n\n"
-                "Asegúrate de que cada bullet siga la estructura de 'Conexión + conector + Beneficio', y evita incluir explicaciones como 'Conexión: Mejorar' o 'Beneficio: Aumentar mi felicidad'.\n"
-                "Importante: Solo responde con bullets, nunca incluyas explicaciones o categorías, como este ejemplo: 'Asistir a la masterclass y descubrir técnicas para potenciar mi carrera profesional. (Este bullet apela al deseo de crecimiento personal y profesional.)'\n"
-                "Los bullets deben de ser variados, basate en estos ejemplos para realizar tu tarea de crear bullets:\n\n"
-                f"Al responder, siempre incluye un encabezado que haga referencia a {target_audience} y el producto de la siguiente manera: "
-                f"'Aquí tienes {num_bullets} bullets para {target_audience}, que aumenten el deseo de adquirir el {product}, usando la mención indirecta:' "
-            )
+    # Configuración del modelo generativo y las instrucciones del sistema
+    model = genai.GenerativeModel(
+        model_name=model_choice,  # Nombre del modelo que estamos utilizando
+        generation_config={
+            "temperature": temperature,  # Cambiamos creatividad por temperature
+            "top_p": 0.85,       
+            "top_k": 128,        
+            "max_output_tokens": 2048,
+            "response_mime_type": "text/plain",
+        },
+        system_instruction=(
+            f"You are a world-class copywriter, expert in creating benefits that connect symptoms with problems of {target_audience}. "
+            f"You deeply understand the emotions, desires, and challenges of {target_audience}, allowing you to design personalized copywriting that resonate and motivate action. "
+            f"You know how to use proven structures to attract {target_audience}, generating interest and creating a powerful connection with {product}. "
+            "Generate unusual, creative, and fascinating bullets that capturing {target_audience}'s attention. Respond in Spanish and use a numbered list format. "
+            "Important: Never include explanations or categories, like this: 'La leyenda del padre soltero: Dice que nunca hay tiempo suficiente. El yoga te enseña a usar mejor el tiempo que tienes, incluso cuando te parece imposible.' "
+            "Los bullets deben ser de los siguientes tipos: "
+            "* 'El armario del baño es el mejor lugar para guardar medicamentos, ¿verdad? Incorrecto. Es el peor. Los hechos están en la página 10.' "
+            "* 'El mejor tiempo verbal que le da a tus clientes la sensación de que ya te han comprado.' "
+            "* 'La historia de...', 'Los misterios de...', 'La leyenda de...' "
+            "* 'Un sistema simple para escribir textos sin intentar convencerlos de comprar.' "
+            "* Verdad: 'La verdad que nunca te han contado en la escuela, o en casa, sobre cómo ganarte la vida con la música.' "
+            "* '¿Sabías que...' "
+            "* '¿Cuándo es una buena idea decirle a una chica que te gusta? Si no se lo dices en ese momento, despídete de conocerla íntimamente.' "
+            "Recuerda que un buen bullet debe tener:\n\n"
+            "1. **Conexión**: Palabras que resalten la relación entre el producto y el beneficio para el usuario (e.g., 'Mejorar', 'Transformar').\n"
+            "2. **Beneficio**: Explicar cómo el usuario se beneficiará al asistir, descargar o comprar el producto.\n\n"
+            "Asegúrate de que cada bullet siga la estructura de 'Conexión + conector + Beneficio', y evita incluir explicaciones como 'Conexión: Mejorar' o 'Beneficio: Aumentar mi felicidad'.\n"
+            "Importante: Solo responde con bullets, nunca incluyas explicaciones o categorías, como este ejemplo: 'Asistir a la masterclass y descubrir técnicas para potenciar mi carrera profesional. (Este bullet apela al deseo de crecimiento personal y profesional.)'\n"
+            "Los bullets deben de variados, basate en estos ejemplos para realizar tu tarea de crear bullets:\n\n"
+            "Usa estos lineamientos para generar bullets de alta conversión en español."
+            f"Al responder, siempre incluye un encabezado que haga referencia a {target_audience} y el producto de la siguiente manera: "
+            f"'Aquí tienes 5 bullets para {target_audience}, que aumenten el deseo de adquirir el {product}, usando la mención indirecta:' "
         )
+    )
 
-        # Crear el prompt para generar bullets
-        bullets_instruction = (
-            f"Tu tarea es escribir {num_bullets} bullets que denoten los beneficios del {product} y que tienen la cualidad de fascinar y por lo tanto, fomentan el deseo de adquirir, asistir, descargar o comprar el {product}. "
-            f"Un buen bullet conecta los síntomas con los problemas enfrentados por {target_audience} de una manera natural, que no se note como manipuladora. "
-            f"Escribe bullets creativos, en un estilo conversacional, que no sean aburridos, sino más bien divertidos. "
-            f"Utiliza la función {mention_instruction} al crear los bullets para referirte a los beneficios del {product}. "
-            "Por favor, crea los bullets ahora."
-        )
+    # Crear el prompt para generar bullets
+    bullets_instruction = (
+        f"Tu tarea es escribir {num_bullets} bullets que denoten los beneficios del {product} y que tienen la cualidad de fascinar y por lo tanto, fomentan el deseo de adquirir, asistir, descargar o comprar el {product}."
+        f"Un buen bullet conecta los síntomas con los problemas enfrentado por {target_audience} de una manera natural, que no se note como manipuladora."
+        f"Escribe bullets creativos, en un estilo conversacional, que no sean aburridos, sino mas bien divertidos. "
+        f"Utiliza la función {mention_instruction} al crear los bullets para referirte a los beneficios del {product}. "
+        "Por favor, crea los bullets ahora."
+    )
 
-        # Crear un mensaje para el modelo que incluye los bullets generados según los tipos seleccionados
-        full_prompt = f"{bullets_instruction}"
+    # Crear un mensaje para el modelo que incluye los bullets generados según los tipos seleccionados
+    full_prompt = f"{bullets_instruction}"
 
-        response = model.generate_content([full_prompt])
+    response = model.generate_content([full_prompt])
 
-        if response and response.parts:
-            bullet = response.parts[0].text.strip()
-            
-            # Verificar si el bullet ya fue generado
-            if bullet not in generated_bullets_set:
-                bullets.append(bullet)
-                generated_bullets_set.add(bullet)  # Agregar bullet al conjunto
-        else:
-            raise ValueError("Lo sentimos, intenta con una combinación diferente de entradas.")
-
-    return "\n".join(bullets)
+    if response and response.parts:
+        return response.parts[0].text
+    else:
+        raise ValueError("Lo sentimos, intenta con una combinación diferente de entradas.")
 
 # Inicializar la aplicación Streamlit
 st.set_page_config(page_title="Generador de Bullets", layout="wide")
@@ -156,13 +145,12 @@ if submit:
             # Obtener la respuesta del modelo
             generated_bullets = get_gemini_response_bullets(target_audience, product, num_bullets, temperature)
             col2.markdown(f"""
-                <div style="border: 2px solid #FFD700; padding: 10px; border-radius: 10px;">
-                    <h3 style='text-align: center;'>Bullets Generados</h3>
-                    <pre>{generated_bullets}</pre>
+                <div style="border: 1px solid #000000; padding: 5px; border-radius: 8px; background-color: #ffffff;">
+                    <h4>Observa la magia en acción:</h4>
+                    <p>{generated_bullets}</p>
                 </div>
-                """, unsafe_allow_html=True)
+            """, unsafe_allow_html=True)
         except ValueError as e:
-            col2.error(str(e))
+            col2.error(f"Error: {str(e)}")
     else:
-        col2.warning("Por favor, completa todos los campos.")
-
+        col2.error("Por favor, proporciona el público objetivo y el producto.")
