e00b34422aa947fc788407e5950fbc3c612f56cd
Jesus Cabrera <JeCabrera@users.noreply.huggingface.co>
2024-09-30 02:55:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,60 @@
+from dotenv import load_dotenv
+import streamlit as st
+import os
+import google.generativeai as genai
+import random
+
+# Cargar las variables de entorno
+load_dotenv()
+
+# Configurar la API de Google
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Función para obtener una mención del producto de manera probabilística
+def get_random_product_mention():
+    mentions = ["Directa", "Indirecta", "Metafórica"]
+    probabilities = [0.35, 0.25, 0.40]
+    return random.choices(mentions, probabilities)[0]
+
+# Crear la instrucción de mención basada en la opción seleccionada
+def get_mention_instruction(product_mention, product):
+    if product_mention == "Directa":
+        return f"Introduce directamente el producto '{product}' como la solución clara al problema que enfrenta el lector, de manera conversacional, no forzada."
+    elif product_mention == "Indirecta":
+        return f"Referencia sutilmente el producto '{product}' como una posible solución al problema del lector sin nombrarlo explícitamente."
+    elif product_mention == "Metafórica":
+        return f"Introduce el producto '{product}' usando una metáfora, conectándolo simbólicamente a la solución que necesita el lector."
+    return ""
+
+# Ejemplos de bullets
+benefit_types = {
+    "directos": [
+        "El armario del baño es el mejor lugar para guardar medicamentos, ¿verdad? Incorrecto. Es el peor. Los hechos están en la página 10.",
+        "El mejor tiempo verbal que le da a tus clientes la sensación de que ya te han comprado.",
+        "La historia de un joven emprendedor que transformó su vida aplicando esta técnica simple pero poderosa."
+    ],
+    "misterios": [
+        "Los misterios de cómo algunas personas parecen tener éxito sin esfuerzo, mientras otras luchan. La clave está en esta pequeña diferencia.",
+        "Los misterios de cómo una técnica sencilla te permite reducir el estrés al instante, sin necesidad de dejar tu trabajo o cambiar tu estilo de vida."
+    ],
+    "leyendas": [
+        "La leyenda de aquellos que dominaron la productividad con un solo hábito. ¿Te atreves a descubrirlo?",
+        "La verdad que nunca te han contado en la escuela, o en casa, sobre cómo ganarte la vida con la música."
+    ],
+    "historias_personales": [
+        "La historia de un padre ocupado que, con solo 10 minutos al día, logró transformar su salud y bienestar.",
+        "¿Sabías que muchas personas están usando este método y han mejorado su bienestar en solo 7 días?"
+    ],
+    "preguntas_retoricas": [
+        "¿Cuándo es una buena idea decirle a una chica que te gusta? Si no se lo dices en ese momento, despídete de conocerla íntimamente."
+    ],
+}
+
+# Generar el resultado utilizando el modelo con la instrucción de bullets específica
 def generate_bullets(number_of_bullets, target_audience, product, call_to_action, temperature):
+    product_mention = get_random_product_mention()
+    mention_instruction = get_mention_instruction(product_mention, product)
+
     # Configuración del modelo
     generation_config = {
         "temperature": temperature,
@@ -8,44 +64,97 @@ def generate_bullets(number_of_bullets, target_audience, product, call_to_action
         "response_mime_type": "text/plain",
     }
 
+    # Crear la instrucción del sistema
+    system_instruction = (
+        f"Eres un experto copywriter especializado en escribir bullets atractivos, curiosos e inusuales para {target_audience} sobre {product} que promueven la acción de {call_to_action}. "
+        f"Tu tarea es ayudarme a escribir {number_of_bullets} bullets que destaquen los beneficios de {product}. "
+        f"Basate en este ejemplo como respuesta, escribe {number_of_bullets} bullets enfocados a beneficios:"
+        "El Curso online de Yoga es tu brújula para navegar las aguas turbulentas de la paternidad soltera."
+        "* Reduce el estrés y la ansiedad como un ancla que te mantiene firme en medio de la tormenta."
+        "* Aumenta tu energía y concentración para navegar con mayor seguridad y precisión."
+        "* Mejora tu flexibilidad y movilidad para adaptarte a cualquier situación con mayor agilidad."
+        "* Encuentra la paz interior como un faro que te guía hacia la calma en medio del caos."
+        "* Conecta contigo mismo para descubrir tu propio rumbo y navegar con mayor confianza."
+        "* Aprende técnicas para gestionar el tiempo y la energía para optimizar tu viaje y disfrutar de cada momento."
+        "Cuando respondas utiliza la mayor cantidad de variaciones"
+    )
+
     # Configuración del modelo generativo
     model = genai.GenerativeModel(
         model_name="gemini-1.5-flash",
         generation_config=generation_config,
     )
 
-    # Definición de system_instruction
-    system_instruction = (
-        f"Eres un experto copywriter especializado en escribir mensajes o textos que atraen la atención de {target_audience} para promover {product} que soluciona los problemas de {target_audience}. "
-        f"Tu tarea es ayudarme a escribir {number_of_bullets} bullets que destaquen los beneficios de {product}, los cuales utilizaré para mi [página web, landing, correo], "
-        f"teniendo en cuenta los puntos dolorosos de mi {target_audience} y el {product} y la {call_to_action} a realizar."
-        "Recuerda que un buen bullet debe tener:\n\n"
-        f"El efecto tiene que ser de atracción, de fascinar, de dejar con la curiosidad. Es más, se dice que los bullets (o balas) tienen que ser como una herida, cuya única cura sea {call_to_action}."
-        "Haz bullets inusuales, creativos y fascinantes que atrapen la atención que conecten el síntoma de la {target_audience} con el beneficio que van a obtener con {call_to_action}. "
-        "Los bullets deben de ser conversacionales, basate en estos ejemplos para realizar tu tarea de crear los bullets:"
-        "* Bien y mal: 'Botiquín del baño es el mejor lugar para guardar la medicina, ¿verdad? Incorrecto Es el peor. Los hechos están en la página 10.'"
-        "* El mejor/El Peor: 'El mejor tiempo verbal que existe para dar la sensación a tus clientes que ya te han comprado.'"
-        "* Historias: 'La historia del...', 'Los misterios de...', 'La leyenda de...'"
-        "* Truco: 'Un sistema tonto para escribir copy sin tratar de convencer de que me compren' [Aquí se refiere al Mecanismo Único a utilizar].'"
-        "* El de la verdad: 'La verdad que nunca te han dicho en el colegio, la escuela, ni en tu casa de como vivir de la música'"
-        "* Haciendo una pregunta: '¿Sabías que?'"
-        "* Cuando: '¿Cuándo es buena idea decirle a una chica que te gusta? Si no lo dices justo en ese momento, despídete de que la conozcas íntimamente.'"
-        "Importante: Solo responde con bullets, nunca incluyas explicaciones o categorías, así: 'Registrarme ahora y descubrir cómo encontrar un poco de paz en medio del caos.'"
-        "Usa estos lineamientos para generar bullets en español."
-    )    
-
     # Generar el resultado utilizando el modelo
     try:
         response = model.generate_content([system_instruction])
         
-        # Depurar la respuesta
-        st.write("Respuesta del modelo:", response)
-
-        # Acceder al contenido generado correctamente
-        generated_bullets = response.result.candidates[0].content.parts[0].text.strip()
-        
-        return generated_bullets
-
+        # Verificar que la respuesta tenga el formato esperado
+        if response.candidates and response.candidates[0].content.parts:
+            generated_bullets = response.candidates[0].content.parts[0].text.strip()
+            return generated_bullets
+        else:
+            raise ValueError("No se generaron bullets válidos.")
     except Exception as e:
-        st.error(f"Error al generar los bullets: {str(e)}")
-        raise
+        raise ValueError(f"Error al generar los bullets: {str(e)}")
+
+# Configurar la interfaz de usuario con Streamlit
+st.set_page_config(page_title="Quick Prompt", layout="wide")
+
+# Centrar el título y el subtítulo
+st.markdown("<h1 style='text-align: center;'>Impact Bullet Generator</h1>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center;'>Transforma los pensamientos de tu audiencia en balas persuasivas que inspiren a la acción.</h4>", unsafe_allow_html=True)
+
+# Añadir CSS personalizado para el botón
+st.markdown("""
+    <style>
+    div.stButton > button {
+        background-color: #FFCC00;
+        color: black;
+        width: 90%;
+        height: 60px;
+        font-weight: bold;
+        font-size: 22px;
+        text-transform: uppercase;
+        border: 1px solid #000000;
+        border-radius: 8px;
+        display: block;
+        margin: 0 auto;
+    }
+    div.stButton > button:hover {
+        background-color: #FFD700;
+        color: black;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+# Crear columnas
+col1, col2 = st.columns([1, 2])  
+
+# Columnas de entrada
+with col1:
+    target_audience = st.text_input("¿Quién es tu público objetivo?", placeholder="Ejemplo: Estudiantes Universitarios")
+    product = st.text_input("¿Qué producto tienes en mente?", placeholder="Ejemplo: Curso de Inglés")
+    call_to_action = st.text_input("¿Qué acción deseas que tomen?", placeholder="Ejemplo: Inscribirse al curso")
+    number_of_bullets = st.selectbox("Número de bullets", options=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], index=2)
+    temperature = st.slider("Creatividad", min_value=0.0, max_value=1.0, value=0.5, step=0.1)
+
+    # Botón de enviar
+    submit = st.button("Generar Beneficios")
+
+# Mostrar los beneficios generados
+if submit:
+    if target_audience and product and call_to_action:
+        try:
+            # Obtener la respuesta del modelo
+            generated_bullets = generate_bullets(number_of_bullets, target_audience, product, call_to_action, temperature)
+            col2.markdown(f"""
+                <div style="border: 1px solid #000000; padding: 5px; border-radius: 8px; background-color: #ffffff;">
+                    <h4>Mira los bullets generados:</h4>
+                    <p style="font-size: 22px;">{generated_bullets}</p>
+                </div>
+            """, unsafe_allow_html=True)
+        except Exception as e:
+            st.error(f"Error al generar los bullets: {str(e)}")
+    else:
+        st.error("Por favor, completa todos los campos.")
